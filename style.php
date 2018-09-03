8603b02afbda7aeb03c2fc9c11318845375da74b
rizkamala <rizkaminion13@gmail.com>
2018-09-03 08:27:10 +0700

    first commit

---
+++ style.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="animation.css">
+
+	  <title>FOOD and DRINK</title>
+	</head>
+	<body>
+	<div class="container"></div>
+
+      <!-- Navigation -->
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+       </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+	            </li>
+	             <li class="nav-item">
+	              <a class="nav-link" href="#">Gallery</a>
+	             </li>
+	            <li class="nav-item">
+	           <a class="nav-link" href="#">Contact</a>
+              </div>
+           	 </li>
+         	</ul>
+     	   </div>
+    	  </nav>
+		<div class="jumbotron jumbotron-fluid">
+		  	<div class="container">
+		    	<h1 class="display-4">MENU MAKANAN</h1>
+			  	<p class="lead">FOOD AND DRINK</p>
+			  	<h1 class="animated wobble heartBeat delay-1s">Selamat Makan</h1>
+			</div>
+		</div>
+
+				    <div class="container">
+					  <div class="row">
+					  	<!-- kolom 1 -->
+					    <div class="col-md">
+			                <table class="table">
+							  <thead class="thead-dark">
+							    <tr>
+							      <th scope="col">NO</th>
+							      <th scope="col">MAKANAN</th>
+							      <th scope="col">HARGA</th>
+							    </tr>
+							  </thead>
+							  <tbody>
+							    <tr>
+							      <th scope="row">1</th>
+							      <td>Nasi Goreng</td>
+							      <td>Rp15.000</td>
+							    </tr>
+							    <tr>
+							      <th scope="row">2</th>
+							      <td>Ayam Geprek</td>
+							      <td>Rp12.000</td>
+							    </tr>
+							    <tr>
+							      <th scope="row">3</th>
+							      <td>Soto Babat</td>
+							      <td>Rp16.000</td>
+							    </tr>
+							    <tr>
+							      <th scope="row">4</th>
+							      <td>Fried Chiken</td>
+							      <td>Rp15.000</td>
+							    </tr>
+							    <tr>
+							      <th scope="row">5</th>
+							      <td>Nasi Tumpeng</td>
+							      <td>Rp35.000</td>
+							    </tr>
+							    <tr>
+							      <th scope="row">6</th>
+							      <td>Ikan Bakar</td>
+							      <td>Rp25.000</td>
+							    </tr>
+							    <tr>
+							      <th scope="row">7</th>
+							      <td>Pecel Lele</td>
+							      <td>Rp18.000</td>
+							    </tr>
+							    <tr>
+							      <th scope="row">8</th>
+							      <td>Sup Daging</td>
+							      <td>Rp12.000</td>
+							    </tr>
+							    <tr>
+							      <th scope="row">9</th>
+							      <td>Nasi Bakar</td>
+							      <td>Rp20.000</td>
+							    </tr>
+							    <tr>
+							      <th scope="row">10</th>
+							      <td>Iga penyet</td>
+							      <td>Rp30.000</td>
+							    </tr>
+							  </tbody>
+							 </table>
+					    </div>
+
+					    <!-- kolom 2 -->
+					    <div class="col-sm">
+							<table class="table">
+							  <thead class="thead-dark">
+							    <tr>
+							      <th scope="col">NO</th>
+							      <th scope="col">MINUMAN</th>
+							      <th scope="col">HARGA</th>
+							      </tr>
+							  </thead>
+						  	  <tbody>
+							    <tr>
+							      <th scope="row">1</th>
+							      <td>ES JERUK</td>
+							      <td>Rp5.000</td>
+							     </tr>
+							     <tr>
+							      <th scope="row">2</th>
+							      <td>ES TEH</td>
+							      <td>Rp3.000</td>
+							     </tr>
+							     <tr>
+							      <th scope="row">3</th>
+							      <td>ES TELER</td>
+							      <td>Rp10.000</td>
+							     </tr>
+							     <tr>
+							      <th scope="row">4</th>
+							      <td>ES BUAH</td>
+							      <td>Rp8.000</td>
+							     </tr>
+							     <tr>
+							      <th scope="row">5</th>
+							      <td>ES KELAPA MUDA</td>
+							      <td>Rp15.000</td>
+							     </tr>
+							     <tr>
+							      <th scope="row">6</th>
+							      <td>JUS JERUK</td>
+							      <td>Rp10.000</td>
+							     </tr>
+							     <tr>
+							      <th scope="row">7</th>
+							      <td>JUS MANGGA</td>
+							      <td>Rp10.000</td>
+							    </tr>
+							     <tr>
+							      <th scope="row">8</th>
+							      <td>JUS NANAS</td>
+							      <td>Rp8.000</td>
+							     </tr>
+							     <tr>
+							      <th scope="row">9</th>
+							      <td>JUS MELON</td>
+							      <td>Rp9.000</td>
+							     </tr>
+							     <tr>
+							      <th scope="row">10</th>
+							      <td>JUS ALPUKAT</td>
+							      <td>Rp7.000</td>
+							     </tr>
+						    </tbody>
+						  </table>
+					    </div>
+					  </div>
+					</div>
+
+		<!-- Modal -->
+		<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		  <div class="modal-dialog" role="document">
+		    <div class="modal-content">
+		      <div class="modal-header">
+		        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+		        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+		          <span aria-hidden="true">&times;</span>
+		        </button>
+		      </div>
+		      <div class="modal-body">
+		        ...
+		      </div>
+		      <div class="modal-footer">
+		        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+		        <button type="button" class="btn btn-primary">Save changes</button>
+		      </div>
+		    </div>
+		  </div>
+		</div>  
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+</body>
+</html>
